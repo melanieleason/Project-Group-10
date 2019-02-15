424741528193b617aec2f984a70c05eb9d63ece3
hsalgi100 <helen.salgi@gmail.com>
2019-02-14 21:08:10 -0600

    Update extract_words.py
    Updated to run for all three script files

--- extract_words.py
+++ extract_words.py
@@ -1,21 +1,45 @@
 import re
+from collections import Counter
+from pprint import pprint
 
 def main():
+	with open("PeterPan.txt", encoding='latin-1') as f:
+		text = f.read().lower()
+		words = re.findall('[a-z]+', text)
+	
+	# pprint(words)
 
-	in_file = open("PeterPan.txt", "r")
-	text  = in_file.read()
-	text = text.lower()
-	text = text.split()
+	with open("allwords.txt", "w", encoding = "latin-1") as f:
+		for word in words:
+			f.write(word + '\n')
+	
+	with open("uniquewords.txt", "w", encoding = "latin-1") as f:
+		cnt = Counter()
+		for word in words:
+			cnt[word] += 1
+		unique_words = []
+		# pprint(cnt.items())
+		for k, v in cnt.items():
+			if v == 1:
+				unique_words.append(k)
+		# pprint(unique_words)
+		for word in unique_words:
+			f.write(word + "\n")
+	
+	with open("wordfrequency.txt", "w", encoding = "latin-1") as f:
+		# frequency: number of words with that frequency
+		freq_words = {v: 0 for v in cnt.values()}
+		for k, v in freq_words.items():
+			for v1 in cnt.values():
+				if k == v1:
+					freq_words[k] +=1
+		# print(freq_words)
+		for k, v in freq_words.items():
+			f.write(str(k) + ": " + str(v) + "\n")
+		
 
-	text1 = []
-	for i in text:
-		i = re.findall('[a-z]+', i)
-		text1.append(i)
-
-	print(text1)
-
-	in_file.close()
 
 
 
-main()
+if __name__ == "__main__":
+	main()
