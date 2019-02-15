b48a738f18e4375779673918b77ee15242c61007
hsalgi100 <helen.salgi@gmail.com>
2019-02-14 21:08:47 -0600

    Update allwords.py

--- allwords.py
+++ allwords.py
@@ -12,30 +12,6 @@ def main():
 	with open("allwords.txt", "w", encoding = "latin-1") as f:
 		for word in words:
 			f.write(word + '\n')
-	
-	with open("uniquewords.txt", "w", encoding = "latin-1") as f:
-		cnt = Counter()
-		for word in words:
-			cnt[word] += 1
-		unique_words = []
-		# pprint(cnt.items())
-		for k, v in cnt.items():
-			if v == 1:
-				unique_words.append(k)
-		# pprint(unique_words)
-		for word in unique_words:
-			f.write(word + "\n")
-	
-	with open("wordfrequency.txt", "w", encoding = "latin-1") as f:
-		# frequency: number of words with that frequency
-		freq_words = {v: 0 for v in cnt.values()}
-		for k, v in freq_words.items():
-			for v1 in cnt.values():
-				if k == v1:
-					freq_words[k] +=1
-		# print(freq_words)
-		for k, v in freq_words.items():
-			f.write(str(k) + ": " + str(v) + "\n")
 		
 
 
